5286b2999f66c36ff7f73760cc8b6fe90f804fce
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-21 23:50:06 +0700

    ADD - bagian interface JenisTrapiService

--- app/src/main/java/com/example/spa/repository/JenisTrapiService.kt
+++ app/src/main/java/com/example/spa/repository/JenisTrapiService.kt
@@ -1,2 +1,17 @@
 package com.example.spa.repository
 
+import com.example.spa.model.AllJenisTrapiResponse
+import com.example.spa.model.JenisTrapi
+
+interface JenisTrapiService {
+    suspend fun insertJenisTrapi(jenisTrapi: JenisTrapi)
+
+    suspend fun updateJenisTrapi(idjenistrapi: String, jenisTrapi: JenisTrapi)
+
+    suspend fun deleteJenisTrapi(idjenistrapi: String)
+
+    suspend fun getJenisTrapi(): AllJenisTrapiResponse
+
+    suspend fun getJenisTrapibyIdJenisTrapi(idjenistrapi: String): JenisTrapi
+}
+
