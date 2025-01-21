ecdd8f17b7e7d98b8f867d90ea1a6cb6b77643e3
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-21 23:22:21 +0700

    ADD - finish class NetworkKontakPasienRepository

--- app/src/main/java/com/example/spa/repository/PasienRepository.kt
+++ app/src/main/java/com/example/spa/repository/PasienRepository.kt
@@ -2,6 +2,8 @@ package com.example.spa.repository
 
 import com.example.spa.model.AllPasienResponse
 import com.example.spa.model.Pasien
+import com.example.spa.service_api.PasienService
+import okio.IOException
 
 interface PasienRepository {
     suspend fun insertPasien(pasien: Pasien)
@@ -13,5 +15,41 @@ interface PasienRepository {
     suspend fun getPasien(): AllPasienResponse
 
     suspend fun getPasienbyIdPasien(idpasien: String): Pasien
+}
+
+class NetworkKontakPasienRepository(
+    private val kontakPasienApiService: PasienService
+): PasienRepository {
+
+    override suspend fun insertPasien(pasien: Pasien) {
+        kontakPasienApiService.insertPasien(pasien)
+    }
+
+    override suspend fun getPasien(): AllPasienResponse {
+        return kontakPasienApiService.getAllPasien()
+    }
+
+    override suspend fun updatePasien(idpasien: String, pasien: Pasien) {
+        kontakPasienApiService.updatePasien(idpasien, pasien)
+    }
+
+    override suspend fun deletePasien(idpasien: String) {
+        try {
+            val response = kontakPasienApiService.deletePasien(idpasien)
+            if (!response.isSuccessful){
+                throw IOException("Failed to delete kontak. HTTP Status code:\" + \"${(response.code())}")
+            }else{
+                response.message()
+                println(response.message())
+            }
+        }
+        catch (e: Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getPasienbyIdPasien(idpasien: String): Pasien {
+        return kontakPasienApiService.getPasienbyIdPasien(idpasien).data
+    }
 }
 
