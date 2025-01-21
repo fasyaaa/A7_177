0806a67d5149b68c526803abd368fbddc3429596
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-21 22:34:49 +0700

    ADD - pasien.kt dan fungsinya selesai

--- app/src/main/java/com/example/spa/model/pasien.kt
+++ app/src/main/java/com/example/spa/model/pasien.kt
@@ -1,4 +1,27 @@
 package com.example.spa.model
 
-class pasien {
-}
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AllPasienResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Pasien>
+)
+
+@Serializable
+data class PasienDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Pasien>
+)
+
+@Serializable
+data class Pasien(
+    val id_pasien: String,
+    val nama_pasien: String,
+    val alamat: String,
+    val no_telp: String,
+    val tgl_lahir: String,
+    val riwayat_medis: String
+)
