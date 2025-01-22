bf10637e21ae1d755ef62fb95cbbac7ac780f863
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-23 03:17:09 +0700

    ADD - Created SesiHomeViewModel to manage Sesi list UI state and handle data fetching

--- app/src/main/java/com/example/spa/ui/sesi/home/viewmodel/SesiHomeViewModel.kt
+++ app/src/main/java/com/example/spa/ui/sesi/home/viewmodel/SesiHomeViewModel.kt
@@ -1,2 +1,40 @@
 package com.example.spa.ui.sesi.home.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.spa.model.Sesi
+import com.example.spa.repository.SesiRepository
+import kotlinx.coroutines.launch
+
+sealed class SesiHomeUiState{
+    data class Success(val sesi: List<Sesi>): SesiHomeUiState()
+    object Error : SesiHomeUiState()
+    object Loading : SesiHomeUiState()
+}
+
+class SesiHomeViewModel (private val ss: SesiRepository)
+    : ViewModel(){
+    var ssUiState: SesiHomeUiState by mutableStateOf(SesiHomeUiState.Loading)
+        private set
+
+    init {
+        getSs()
+    }
+
+    fun getSs(){
+        viewModelScope.launch {
+            ssUiState = SesiHomeUiState.Loading
+            ssUiState = try {
+                SesiHomeUiState.Success(ss.getSesi().data)
+            } catch (e: Exception){
+                SesiHomeUiState.Error
+            } catch (e: HttpException){
+                SesiHomeUiState.Error
+            }
+        }
+    }
+}
