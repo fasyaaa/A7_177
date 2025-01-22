60b0cf2877fa027fe096f71c1c47ae684137f8b1
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-22 23:15:25 +0700

    ADD - Implemented TerapisViewModel for updating Terapis data with state management.

---
+++ app/src/main/java/com/example/spa/ui/terapis/update/viewmodel/TerapisViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.spa.ui.terapis.update.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.spa.repository.TerapisRepository
+import com.example.spa.ui.terapis.insert.viewmodel.TerapisInsertUiEvent
+import com.example.spa.ui.terapis.insert.viewmodel.TerapisInsertUiState
+import com.example.spa.ui.terapis.insert.viewmodel.toTer
+import com.example.spa.ui.terapis.insert.viewmodel.toUiStateTer
+import kotlinx.coroutines.launch
+
+class TerapisViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val ter: TerapisRepository
+): ViewModel(){
+    var terapisUpdateUiState by mutableStateOf(TerapisInsertUiState())
+        private set
+
+    private val _id_terapis: String = checkNotNull(savedStateHandle[DestinasiTerapisUpdate.IdTerapis])
+
+    init {
+        viewModelScope.launch {
+            terapisUpdateUiState = ter.getTerapisbyIdTerapis(_id_terapis)
+                .toUiStateTer()
+        }
+    }
+
+    fun updateInsertTerState(terapisInsertUiEvent: TerapisInsertUiEvent){
+        terapisUpdateUiState = TerapisInsertUiState(terapisInsertUiEvent = terapisInsertUiEvent)
+    }
+
+    suspend fun updateTer(){
+        viewModelScope.launch {
+            try {
+                ter.updateTerapis(_id_terapis, terapisUpdateUiState.terapisInsertUiEvent.toTer())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
