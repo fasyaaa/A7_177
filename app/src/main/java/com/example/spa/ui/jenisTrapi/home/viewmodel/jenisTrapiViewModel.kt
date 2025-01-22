fe1b8b7db91a215f9995465f1d3a82a8f6803ef8
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-22 23:27:25 +0700

    ADD - Implemented JenisTrapiHomeViewModel to manage JenisTrapi list with UI state handling.

--- app/src/main/java/com/example/spa/ui/jenisTrapi/home/viewmodel/jenisTrapiViewModel.kt
+++ app/src/main/java/com/example/spa/ui/jenisTrapi/home/viewmodel/jenisTrapiViewModel.kt
@@ -1,2 +1,39 @@
 package com.example.spa.ui.jenisTrapi.home.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.spa.model.JenisTrapi
+import com.example.spa.repository.JenisTrapiRepository
+import kotlinx.coroutines.launch
+import okio.IOException
+
+sealed class JenisTrapiHomeUiState{
+    data class Success(val jenisTrapi: List<JenisTrapi>): JenisTrapiHomeUiState()
+    object Error: JenisTrapiHomeUiState()
+    object Loading: JenisTrapiHomeUiState()
+}
+
+class JenisTrapiHomeViewModel(private val jtr: JenisTrapiRepository): ViewModel(){
+    var jtrUiState: JenisTrapiHomeUiState by mutableStateOf(JenisTrapiHomeUiState.Loading)
+        private set
+
+    init {
+        getJtr()
+    }
+
+    fun getJtr(){
+        viewModelScope.launch {
+            jtrUiState = JenisTrapiHomeUiState.Loading
+            jtrUiState = try {
+                JenisTrapiHomeUiState.Success(jtr.getJenisTrapi().data)
+            } catch (e: IOException){
+                JenisTrapiHomeUiState.Error
+            } catch (e: IOException){
+                JenisTrapiHomeUiState.Error
+            }
+        }
+    }
+}
