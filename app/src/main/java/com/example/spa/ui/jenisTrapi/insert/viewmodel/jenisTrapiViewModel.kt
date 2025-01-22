b460e210240215c0c5a25fb738690955b721e5ea
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-22 23:38:13 +0700

    ADD - Added JenisTrapiInsertViewModel for managing JenisTrapi insertion and state updates

--- app/src/main/java/com/example/spa/ui/jenisTrapi/insert/viewmodel/jenisTrapiViewModel.kt
+++ app/src/main/java/com/example/spa/ui/jenisTrapi/insert/viewmodel/jenisTrapiViewModel.kt
@@ -1,2 +1,43 @@
 package com.example.spa.ui.jenisTrapi.insert.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.spa.model.JenisTrapi
+import com.example.spa.repository.JenisTrapiRepository
+
+class JenisTrapiInsertViewModel(private val jtr: JenisTrapiRepository): ViewModel(){
+    var jenisTrapisUiState by mutableStateOf(JenisTrapiInsertUiState())
+        private set
+
+    fun updateInsertJtrState(jenisTrapiInsertUiEvent: JenisTrapiInsertUiEvent){
+        jenisTrapisUiState = JenisTrapiInsertUiState(jenisTrapiInsertUiEvent = jenisTrapiInsertUiEvent)
+    }
+}
+
+data class JenisTrapiInsertUiState(
+    val jenisTrapiInsertUiEvent: JenisTrapiInsertUiEvent = JenisTrapiInsertUiEvent()
+)
+
+data class JenisTrapiInsertUiEvent(
+    val id_jenisTrapi: String = "",
+    val nama_jenisTrapi: String = "",
+    val deskripsi_jenisTrapi: String = ""
+)
+
+fun JenisTrapiInsertUiEvent.toJtr(): JenisTrapi = JenisTrapi(
+    id_jenisTrapi = id_jenisTrapi,
+    nama_jenisTrapi = nama_jenisTrapi,
+    deskripsi_jenisTrapi = deskripsi_jenisTrapi
+)
+
+fun JenisTrapi.toUiStateJtr(): JenisTrapiInsertUiState = JenisTrapiInsertUiState(
+    jenisTrapiInsertUiEvent = toJenisTrapiEvent()
+)
+
+fun JenisTrapi.toJenisTrapiEvent(): JenisTrapiInsertUiEvent = JenisTrapiInsertUiEvent(
+    id_jenisTrapi = id_jenisTrapi,
+    nama_jenisTrapi = nama_jenisTrapi,
+    deskripsi_jenisTrapi = deskripsi_jenisTrapi
+)
