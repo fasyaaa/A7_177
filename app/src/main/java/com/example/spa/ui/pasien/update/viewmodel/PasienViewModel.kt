51c699602cb795d4b05f393151f234e86d2943d6
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-22 13:41:35 +0700

    ADD -  Added UpdatePasienViewModel for updating Pasien data with state management

--- app/src/main/java/com/example/spa/ui/pasien/update/viewmodel/PasienViewModel.kt
+++ app/src/main/java/com/example/spa/ui/pasien/update/viewmodel/PasienViewModel.kt
@@ -1,2 +1,45 @@
 package com.example.spa.ui.pasien.update.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.spa.repository.PasienRepository
+import com.example.spa.ui.pasien.insert.viewmodel.PasInsertUiEvent
+import com.example.spa.ui.pasien.insert.viewmodel.PasInsertUiState
+import com.example.spa.ui.pasien.insert.viewmodel.toPas
+import com.example.spa.ui.pasien.insert.viewmodel.toUiStatePas
+import kotlinx.coroutines.launch
+
+class UpdatePasienViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val pas: PasienRepository
+): ViewModel(){
+    var pasienUpdateUiState by mutableStateOf(PasInsertUiState())
+        private set
+
+    private val _id_pasien: String = checkNotNull(savedStateHandle[DestinasiUpdate.IdPasien])
+
+    init {
+        viewModelScope.launch {
+            pasienUpdateUiState = pas.getPasienbyIdPasien(_id_pasien)
+                .toUiStatePas()
+        }
+    }
+
+    fun updateInsertPasState(insertUiState: PasInsertUiEvent){
+        pasienUpdateUiState = PasInsertUiState(pasInsertUiEvent = insertUiState)
+    }
+
+    suspend fun updatePas(){
+        viewModelScope.launch {
+            try {
+                pas.updatePasien(_id_pasien, pasienUpdateUiState.pasInsertUiEvent.toPas())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
