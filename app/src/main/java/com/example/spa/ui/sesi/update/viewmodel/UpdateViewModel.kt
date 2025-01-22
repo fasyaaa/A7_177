961563c6c89a0a5de5ae806f065007ca99fffc41
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-23 04:21:16 +0700

    ADD - Created SesiUpdateViewModel for updating Sesi data

--- app/src/main/java/com/example/spa/ui/sesi/update/viewmodel/UpdateViewModel.kt
+++ app/src/main/java/com/example/spa/ui/sesi/update/viewmodel/UpdateViewModel.kt
@@ -1,2 +1,45 @@
 package com.example.spa.ui.sesi.update.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.spa.repository.SesiRepository
+import com.example.spa.ui.sesi.insert.viewmodel.SesiInsertUiEvent
+import com.example.spa.ui.sesi.insert.viewmodel.SesiInsertUiState
+import com.example.spa.ui.sesi.insert.viewmodel.toSs
+import com.example.spa.ui.sesi.insert.viewmodel.toUiStateSs
+import kotlinx.coroutines.launch
+
+class SesiUpdateViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val ss: SesiRepository
+) : ViewModel(){
+    var sesiUpdateUiState by mutableStateOf(SesiInsertUiState())
+        private set
+
+    private val _id_sesi: String = checkNotNull(savedStateHandle[DestinasiSesiUpdate.IdSesi])
+
+    init {
+        viewModelScope.launch {
+            sesiUpdateUiState = ss.getSesibyIdSesi(_id_sesi)
+                .toUiStateSs()
+        }
+    }
+
+    fun updateInsertSsState(sesiInsertUiEvent: SesiInsertUiEvent){
+        sesiUpdateUiState = SesiInsertUiState(sesiInsertUiEvent = sesiInsertUiEvent)
+    }
+
+    suspend fun updateSs(){
+        viewModelScope.launch {
+            try {
+                ss.updateSesi(_id_sesi, sesiUpdateUiState.sesiInsertUiEvent.toSs())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
