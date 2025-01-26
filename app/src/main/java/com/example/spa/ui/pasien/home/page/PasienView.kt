145b96db70bffb493a761fdacc0b6fefd2deaeca
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-27 06:21:20 +0700

    UPD - Delete not use import

--- app/src/main/java/com/example/spa/ui/pasien/home/page/PasienView.kt
+++ app/src/main/java/com/example/spa/ui/pasien/home/page/PasienView.kt
@@ -22,7 +22,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
