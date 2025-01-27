e021ea8f63b592a4306a9913e73d35ef64f8d131
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-27 15:37:16 +0700

    ADD - SesiView for Detail + Home

---
+++ app/src/main/java/com/example/spa/ui/sesi/detail/page/SesiView.kt
@@ -0,0 +1,184 @@
+package com.example.spa.ui.sesi.detail.page
+
+import CoustumeTopAppBar
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.spa.model.Pasien
+import com.example.spa.model.Sesi
+import com.example.spa.ui.navigation.DestinasiNavigasi
+import com.example.spa.ui.pasien.PasienPenyediaViewModel
+import com.example.spa.ui.pasien.detail.viewmodel.PasienDetailUiState
+import com.example.spa.ui.pasien.detail.viewmodel.PasienDetailViewModel
+import com.example.spa.ui.pasien.home.page.OnError
+import com.example.spa.ui.pasien.home.page.OnLoading
+import com.example.spa.ui.sesi.SesiPenyediaViewModel
+import com.example.spa.ui.sesi.detail.viewmodel.SesiDetailUiState
+import com.example.spa.ui.sesi.detail.viewmodel.SesiDetailViewModel
+
+object DestinasiDetailSesi: DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = "Detail Sesi"
+    const val idSesi = "id_sesi"
+    val routesWithArg = "$route/{$idSesi}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSesiScreen(
+    navigateBack: () -> Unit,
+    navigateToItemUpdate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: SesiDetailViewModel = viewModel(factory = SesiPenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            CoustumeTopAppBar(
+                title = DestinasiDetailSesi.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                onRefresh = {
+                    viewModel.getSesibyIdSesi()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemUpdate,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Kontak"
+                )
+            }
+        }
+    ) { innerPadding ->
+        DetailStatusSs(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = viewModel.sesiDetailUiState,
+            retryAction = { viewModel.getSesibyIdSesi()},
+            onDeleteAction = { idSesi ->
+                viewModel.deleteSs(idSesi)
+                navigateBack()
+            }
+        )
+    }
+}
+
+@Composable
+fun DetailStatusSs(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    detailUiState: SesiDetailUiState,
+    onDeleteAction: (Int) -> Unit
+) {
+    when (detailUiState) {
+        is SesiDetailUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+        is SesiDetailUiState.Success -> {
+            if (detailUiState.sesi.idSesi == 0) {
+                Box(
+                    modifier = modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) { Text("Data tidak ditemukan.") }
+            } else {
+                Column(modifier = modifier.fillMaxSize()) {
+                    ItemDetailSs(
+                        sesi = detailUiState.sesi,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    FloatingActionButton(
+                        onClick = { onDeleteAction(detailUiState.sesi.idSesi) },
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Edit,
+                            contentDescription = "Hapus Pasien"
+                        )
+                    }
+                }
+            }
+        }
+        is SesiDetailUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun ItemDetailSs(
+    modifier: Modifier = Modifier,
+    sesi: Sesi
+){
+    Card(
+        modifier = modifier.padding(16.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailSs(judul = "Id Sesi", isinya = sesi.idSesi.toString())
+            Spacer(modifier = Modifier.padding(5.dp))
+            ComponentDetailSs(judul = "Id Pasien", isinya = sesi.idPasien.toString())
+            Spacer(modifier = Modifier.padding(5.dp))
+            ComponentDetailSs(judul = "Id Terapis", isinya = sesi.idTerapis.toString())
+            Spacer(modifier = Modifier.padding(5.dp))
+            ComponentDetailSs(judul = "Id Jenis Terapi", isinya = sesi.idJenisTrapi.toString())
+            Spacer(modifier = Modifier.padding(5.dp))
+            ComponentDetailSs(judul = "Catatan Sesi", isinya = sesi.catatanSesi)
+            Spacer(modifier = Modifier.padding(5.dp))
+            ComponentDetailSs(judul = "Tanggal Sesi", isinya = sesi.tanggalSesi)
+            Spacer(modifier = Modifier.padding(5.dp))
+        }
+    }
+}
+
+@Composable
+fun ComponentDetailSs(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = judul,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
--- app/src/main/java/com/example/spa/ui/sesi/home/page/SesiView.kt
+++ app/src/main/java/com/example/spa/ui/sesi/home/page/SesiView.kt
@@ -1,2 +1,173 @@
 package com.example.spa.ui.sesi.home.page
 
+import CoustumeTopAppBar
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.spa.model.Sesi
+import com.example.spa.ui.navigation.DestinasiNavigasi
+import com.example.spa.ui.pasien.home.page.OnError
+import com.example.spa.ui.pasien.home.page.OnLoading
+import com.example.spa.ui.sesi.SesiPenyediaViewModel
+import com.example.spa.ui.sesi.home.viewmodel.SesiHomeUiState
+import com.example.spa.ui.sesi.home.viewmodel.SesiHomeViewModel
+
+object SesiHome : DestinasiNavigasi {
+    override val route = "home Sesi"
+    override val titleRes = "Home JeT"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeSesiScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit,
+    viewModel: SesiHomeViewModel = viewModel(factory = SesiPenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CoustumeTopAppBar(
+                title = SesiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getSs()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ){
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Kontak")
+            }
+        },
+    ){ innerPadding ->
+        HomeSesiStatus(
+            homeUiState = viewModel.ssUiState,
+            retryAction = { viewModel.getSs() },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick
+        )
+    }
+}
+
+@Composable
+fun HomeSesiStatus(
+    homeUiState: SesiHomeUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit,
+) {
+    when (homeUiState) {
+        is SesiHomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is SesiHomeUiState.Success ->
+            if (homeUiState.sesi.isEmpty()) {
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Kontak")
+                }
+            } else {
+                SesiLayout(
+                    sesi = homeUiState.sesi,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.idSesi.toString())
+                    }
+                )
+            }
+
+        is SesiHomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun SesiLayout(
+    sesi: List<Sesi>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Sesi) -> Unit,
+){
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(sesi){kontak ->
+            SesiCard(
+                sesi = kontak,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        onDetailClick(kontak)},
+            )
+        }
+    }
+}
+
+@Composable
+fun SesiCard(
+    sesi: Sesi,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = sesi.idPasien.toString(),
+                style = MaterialTheme.typography.titleLarge
+            )
+            Text(
+                text = sesi.idTerapis.toString(),
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = sesi.idJenisTrapi.toString(),
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = sesi.catatanSesi,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = sesi.tanggalSesi,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}
