c17fc65ed5dfe03ea610a45e8b14ac26403b1da3
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-28 04:00:25 +0700

    UPD - Change to SpaTheme

--- app/src/main/java/com/example/spa/MainActivity.kt
+++ app/src/main/java/com/example/spa/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.spa.ui.SpaApp
 import com.example.spa.ui.theme.SpaTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SpaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SpaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
